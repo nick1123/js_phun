4c45b164059f5fe27fab6162a87fceb80759a1b0
Stewie (Nicholas Stewart) <nstewart@instructure.com>
2016-07-18 11:45:37 -0600

    Add Triangle script

---
+++ 01_pound_triangle.js
@@ -0,0 +1,11 @@
+// Write a loop that makes seven calls to console.log to output the following triangle`
+var string;
+for(i=1; i<=7; i++){
+  string = '';
+  for(j=1; j<=i; j++){
+    string += '#';
+  }
+
+  console.log(string)
+}
+

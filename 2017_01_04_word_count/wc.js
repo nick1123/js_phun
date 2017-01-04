ef4af0f4edaca9f5d37072f3506670ef8fc78de0
Stewie (Nicholas Stewart) <nstewart@instructure.com>
2017-01-04 13:12:47 -0700

    Conform to es6 conventions

--- 2017_01_04_word_count/wc.js
+++ 2017_01_04_word_count/wc.js
@@ -1,14 +1,5 @@
 // Run this with:
 // node wc.js quotes.txt
-var fileName = process.argv[2];
-fs = require('fs')
-fs.readFile(fileName, 'utf8', function (err, data) {
-    if (err) {
-        return console.log(err);
-    }
-
-    console.log(lineCount(data) + ' ' + wordCount(data) + ' ' + totalChars(data) + ' ' + fileName);
-});
 
 function lineCount(data) {
     return data.trim().split("\n").length - 1;
@@ -21,3 +12,13 @@ function wordCount(data) {
 function totalChars(data) {
     return data.length;
 }
+
+var fileName = process.argv[2];
+fs = require('fs')
+fs.readFile(fileName, 'utf8', function (err, data) {
+    if (err) {
+        return console.log(err);
+    }
+
+    console.log(`${lineCount(data)} ${wordCount(data)} ${totalChars(data)} ${fileName}`);
+});
